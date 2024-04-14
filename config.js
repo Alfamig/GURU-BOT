f8f13770cf0c6f51495c28a1f49ac4cfa6476376
Alfamig <166089740+Alfamig@users.noreply.github.com>
2024-04-14 05:03:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['237674152751', 'migel', true],
+  ['237674152751', 'Alfa', true], 
   [''] 
 ] //Number of owners
 
